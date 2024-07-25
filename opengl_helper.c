5e9a1ba952d5fbebdb124dd21a1e7a51d9030e53
Jeremy Montgomery <thesirirk@gmail.com>
2024-07-25 11:27:37 -0500

    Update opengl_helper.c

--- opengl_helper.c
+++ opengl_helper.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2019 Jeremy Montgomery
+Copyright (c) 2024 Jeremy Montgomery
 Permission is hereby granted, free of charge, to any person obtaining a copy of 
 this software and associated documentation files (the "Software"), to deal in 
 the Software without restriction, including without limitation the rights to 
